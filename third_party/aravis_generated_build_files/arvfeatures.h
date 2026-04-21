d38a94e3f0f5ed784881230373e699d1453f3bf2
Owen Scott <owens3364@gmail.com>
2026-04-21 01:39:40 -0500

    wip

---
+++ third_party/aravis_generated_build_files/arvfeatures.h
@@ -0,0 +1,14 @@
+#ifndef ARV_FEATURES_H
+#define ARV_FEATURES_H
+
+#if !defined(ARV_H_INSIDE) && !defined(ARAVIS_COMPILATION)
+#error "Only <arv.h> can be included directly."
+#endif
+
+#define ARAVIS_HAS_USB 0
+#define ARAVIS_HAS_V4L2 0
+#define ARAVIS_HAS_PACKET_SOCKET 0
+#define ARAVIS_HAS_EVENT 0
+#define ARAVIS_HAS_FAST_HEARTBEAT 0
+
+#endif
---
+++ third_party/aravis_generated_build_files/arvparamsprivate.h
@@ -0,0 +1,4 @@
+#ifndef ARV_PARAMS_H
+#define ARV_PARAMS_H
+#define ARV_GV_STREAM_NUM_BUFFERS 16
+#endif
