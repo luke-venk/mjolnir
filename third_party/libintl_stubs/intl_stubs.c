d9ee1f5c5339f0175776f6b50549b2f8fb2a867f
Owen Scott <owens3364@gmail.com>
2026-04-25 16:24:40 -0500

    fucking malloc

--- third_party/libintl_stubs/intl_stubs.c
+++ third_party/libintl_stubs/intl_stubs.c
@@ -1,5 +1,6 @@
 #include "libintl.h"
 
+/* --- Standard Gettext Stubs --- */
 char *gettext(const char *msgid) { return (char *)msgid; }
 char *dgettext(const char *domain, const char *msgid) { return (char *)msgid; }
 char *dcgettext(const char *domain, const char *msgid, int category) { return (char *)msgid; }
@@ -10,10 +11,34 @@ char *bindtextdomain(const char *domain, const char *dir) { return (char *)domai
 char *bind_textdomain_codeset(const char *domain, const char *codeset) { return (char *)codeset; }
 char *textdomain(const char *domain) { return (char *)domain; }
 
-const gchar *g_dngettext(const gchar *domain,
-                         const gchar *msgid1,
-                         const gchar *msgid2,
-                         gulong n)
+/* --- GLib Specific Gettext Stubs --- */
+const gchar *g_dgettext(const gchar *domain, const gchar *msgid) { return msgid; }
+const gchar *g_dngettext(const gchar *domain, const gchar *m1, const gchar *m2, gulong n)
 {
-  return (n == 1) ? msgid1 : msgid2;
+  return (n == 1) ? m1 : m2;
 }
+const gchar *g_dcgettext(const gchar *domain, const gchar *msgid, int category) { return msgid; }
+
+/* --- macOS Platform Stubs --- */
+void load_user_special_dirs_macos(gchar **special_dirs) { /* No-op */ }
+const gchar *g_content_type_get_icon_impl(const gchar *type) { return NULL; }
+const gchar *g_content_type_get_symbolic_icon_impl(const gchar *type) { return NULL; }
+const gchar *g_content_type_from_mime_type_impl(const gchar *mime) { return NULL; }
+const gchar *g_content_type_guess_impl(const gchar *filename, const guchar *data, size_t data_size, int *result_uncertain) { return NULL; }
+
+/* --- GIO Internal Stubs --- */
+int glib_get_sandbox_type(void) { return 0; }
+gsize g_threaded_resolver_get_type(void) { return 0; }
+gsize _g_local_vfs_get_type(void) { return 0; }
+gpointer _g_local_vfs_new(void) { return NULL; }
+
+/* Additional GIO Module Stubs */
+gsize g_memory_monitor_portal_get_type(void) { return 0; }
+gsize g_memory_monitor_dbus_get_type(void) { return 0; }
+gsize g_memory_monitor_get_type(void) { return 0; }
+gsize g_fdo_notification_backend_get_type(void) { return 0; }
+gsize g_osx_app_info_get_type(void) { return 0; }
+gsize g_nextstep_settings_backend_get_type(void) { return 0; }
+gsize g_cocoa_notification_backend_get_type(void) { return 0; }
+gsize g_kqueue_file_monitor_get_type(void) { return 0; }
+gsize g_keyfile_settings_backend_get_type(void) { return 0; }
--- third_party/libintl_stubs/libintl.h
+++ third_party/libintl_stubs/libintl.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include "glib/glib/gtypes.h"
 
+/* Standard Gettext */
 char *gettext(const char *msgid);
 char *dgettext(const char *domain, const char *msgid);
 char *dcgettext(const char *domain, const char *msgid, int category);
@@ -14,9 +15,36 @@ char *bindtextdomain(const char *domain, const char *dir);
 char *bind_textdomain_codeset(const char *domain, const char *codeset);
 char *textdomain(const char *domain);
 
+/* GLib Specific Gettext - declarations must exist for the C files to compile */
+const gchar *g_dgettext(const gchar *domain, const gchar *msgid);
+const gchar *g_dcgettext(const gchar *domain, const gchar *msgid, int category);
 const gchar *g_dngettext(const gchar *domain,
                          const gchar *msgid1,
                          const gchar *msgid2,
                          gulong n);
+
+/* Platform Impl Stubs */
+void load_user_special_dirs_macos(gchar **special_dirs);
+const gchar *g_content_type_get_icon_impl(const gchar *type);
+const gchar *g_content_type_get_symbolic_icon_impl(const gchar *type);
+const gchar *g_content_type_from_mime_type_impl(const gchar *mime);
+const gchar *g_content_type_guess_impl(const gchar *filename, const guchar *data, size_t data_size, int *result_uncertain);
+
+/* GIO/Sandbox Stubs */
+int glib_get_sandbox_type(void);
+gsize g_threaded_resolver_get_type(void);
+gsize _g_local_vfs_get_type(void);
+gpointer _g_local_vfs_new(void);
+
+/* Additional GIO Module Stubs */
+gsize g_memory_monitor_portal_get_type(void);
+gsize g_memory_monitor_dbus_get_type(void);
+gsize g_memory_monitor_get_type(void);
+gsize g_fdo_notification_backend_get_type(void);
+gsize g_osx_app_info_get_type(void);
+gsize g_nextstep_settings_backend_get_type(void);
+gsize g_cocoa_notification_backend_get_type(void);
+gsize g_kqueue_file_monitor_get_type(void);
+gsize g_keyfile_settings_backend_get_type(void);
 
 #endif
