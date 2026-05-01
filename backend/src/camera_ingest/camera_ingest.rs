0da8bae850a109ed32aaf7442d1af93a3654e456
Owen Scott <owens3364@gmail.com>
2026-05-01 14:58:51 -0500

    wip

--- backend/src/camera_ingest/camera_ingest.rs
+++
@@ -1,9 +0,0 @@
-use crate::pipeline::Frame;
-
-// Ingests frames from the cameras using the GigEVision API, and enqueues
-// the frames into the camera ingest sender for the camera's pipeline to
-// begin processing.
-pub fn ingest_frames(_tx: crossbeam::channel::Sender<Frame>) {
-    // TODO(#3): Implement Camera Ingest.
-    todo!();
-}
